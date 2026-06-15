2aa8e76872cd0f66a0a094aca1676d33722ff44b
Shimar Ahamed <139215165+shimarahamed@users.noreply.github.com>
2026-06-16 01:38:10 +0400

    Delete config.ts

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-7320841525-43d78",
-  "appId": "1:299844441025:web:e18d38e3532d7adf295540",
-  "apiKey": "AIzaSyDQvNp4cgPLjj7T7CR0WQZwFnJuVvf0EX8",
-  "authDomain": "studio-7320841525-43d78.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "299844441025"
-};
